7733be09835eb2e88049f24c31743482e21b40c9
rmckinnes <rpm.7791@yahoo.com>
2018-04-17 16:33:08 -0400

    Update UpdateServlet.java

--- Controllers/UpdateServlet.java
+++ Controllers/UpdateServlet.java
@@ -10,19 +10,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Grocery;
-import dbhelpers.ReadRecord;
+import dbhelpers.UpdateQuery;
 
 /**
- * Servlet implementation class UpdateFormServlet
+ * Servlet implementation class UpdateBookServlet
  */
-@WebServlet(description = "This will get an item and use the data to fill in a table for updating the record", urlPatterns = { "/update" })
-public class UpdateServlet extends HttpServlet {
+@WebServlet(description = "Controller which starts the actual book update to the database", urlPatterns = { "/updateBook" })
+public class UpdateItemServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public UpdateItemServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -38,21 +38,28 @@ public class UpdateServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// get the bookID
+		// get the form data and set up a Book object
 		String sku = request.getParameter("sku");
+		String producttype = request.getParameter("producttype");
+		String flavor = request.getParameter("flavor");
+		double cost = Double.parseDouble(request.getParameter("cost"));
+		double retail = Double.parseDouble(request.getParameter("retail"));
+		int amount = Integer.parseInt(request.getParameter("amount"));
 		
-		// create ReadRecord class
-		ReadRecord rr = new ReadRecord("grocery_list", "root", "admin", sku);
+		Grocery grocery = new Grocery();
+		grocery.setSKU(sku);
+		grocery.setProductType(producttype);
+		grocery.setFlavor(flavor);
+		grocery.setCost(cost);
+		grocery.setRetail(retail);
+		grocery.setAmount(amount);
 		
-		// Use ReadRecord to get the book data
-		rr.doRead();
+		// create an UpdateQuery object and use it to update the book
+		UpdateQuery uq = new UpdateQuery("grocery_list", "root", "admin");
+		uq.doUpdate(grocery);
 		
-		Grocery grocery = rr.getGrocery();
-		
-		// pass Book and control to the updateForm.jsp
-		request.setAttribute("grocery", grocery);
-		
-		String url = "/updateForm.jsp";
+		// pass control on to the ReadServlet
+		String url = "/read";
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
