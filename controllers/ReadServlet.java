7d2f254ff9f4d23450303b975bbaef9af9c871d2
mcawthon <mcawthon18@gmail.com>
2018-04-24 20:12:38 -0400

    Delete Drone Service

--- controllers/ReadServlet.java
+++ controllers/ReadServlet.java
@@ -9,9 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import helpers.ReadQuery;
-
-//import helpers.ReadQuery;
+import dbhelpers.ReadQuery;
 
 /**
  * Servlet implementation class ReadServlet
@@ -22,15 +20,13 @@ import helpers.ReadQuery;
 				"/ReadServlet", 
 				"/read"
 		})
-public class ReadServlet extends HttpServlet 
-{
+public class ReadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ReadServlet()
-    {
+    public ReadServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -38,77 +34,28 @@ public class ReadServlet extends HttpServlet
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{
-		String query = request.getParameter("string");
-		String type = query.substring(0,1);
-		String id = query.substring(1);
-		if(type.equalsIgnoreCase('d'))
-		{
-			// Create a ReadQuery helper object
-			ReadQuery rq = new ReadQuery("droneservice", "root", "admin");
-			rq.readDrone();
-			// Get the html table from the ReadQuery object
-			String table = rq.getDroneHTML();
-			// pass execution control to read.jsp along with the table
-			request.setAttribute("table", table);
-			String url = "/drone.jsp";
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
-		}
-		else if(type.equalsIgnoreCase("s"))
-		{
-			ReadQuery rq = new ReadQuery("droneservice", "root", "admin");
-			rq.readDroneService();
-			
-			String table = rq.getServiceHTML();
-			
-			request.setAttribute("table", table);
-			String url = "/service.jsp";
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
-		}
-		else if(type.equalsIgnoreCase("p"))
-		{
-			ReadQuery rq = new ReadQuery("droneservice", "root", "admin");
-			rq.readPart();
-			
-			String table = rq.getPartHTML();
-			
-			request.setAttribute("table", table);
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
-		}
-		else if(type.equalsIgnoreCase("e"))
-		{
-			ReadQuery rq = new ReadQuery("droneservice", "root", "admin");
-			rq.readEmployee();
-			
-			String table = rq.getEmployeeHTML();
-			
-			request.setAttribute("table", table);
-			String url = "employee.jsp";
-			
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
-		}
-		else
-		{
-			String url = "index.jsp";
-			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-			dispatcher.forward(request, response);
-		}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Create a ReadQuery helper object
+		ReadQuery rq = new ReadQuery("grocery_list", "root", "admin");
+		
+		// Get the html table from the ReadQuery object
+		rq.doRead();
+		String table = rq.getHTMLTable();
+		
+		// pass execution control to read.jsp along with the table
+		request.setAttribute("table", table);
+		String url = "/read.jsp";
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
+		
 	}
 
 }
