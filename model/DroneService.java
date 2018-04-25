0e8ccfee2e9530ccd591a7654d2a667bad88c276
rmckinnes <rpm.7791@yahoo.com>
2018-04-24 20:25:00 -0400

    Update DroneService.java

--- model/DroneService.java
+++ model/DroneService.java
@@ -4,4 +4,48 @@ public class DroneService
 {
 	private int serviceID;
 	private String description;
+	private String serviceDate;
+	
+	
+	
+	
+	public DroneService()
+	{
+		serviceID = 0; 
+		description = "";
+	}
+	
+	
+	public int getServiceID()
+	{
+		return serviceID;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+	
+	public void setServiceID(int serviceID)
+	{
+		this.serviceID = serviceID;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+
+	public String getDate() 
+	{
+		return serviceDate;
+	}
+
+
+	public void setDate(String servicedate) 
+	{
+		
+		this.serviceDate = servicedate;
+	}
 }
